4c22ad8e60a3a5fab9ef6cb26bf1729ec64b34a2
varsha-kushwaha123 <aec.cse.2024varshakushwaha@gmail.com>
2025-08-06 20:50:48 +0530

    Time: 1 ms (81.57%), Space: 40.8 MB (85.74%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        int rev=0,rem;
+
+             while(x!=0){
+                rem=x%10;
+                if(rev<Integer.MIN_VALUE/10||rev>Integer.MAX_VALUE/10){
+                    return 0;
+                }
+                rev=rev*10+rem;
+                x=x/10;
+            }
+        return rev;
+        
+    }
+ }

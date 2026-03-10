d37568a00aad3b3440b81a6c603170c1abf97df6
varsha-kushwaha123 <aec.cse.2024varshakushwaha@gmail.com>
2026-03-10 23:25:12 +0530

    Time: 11 ms (99.43%), Space: 47.8 MB (56.84%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
@@ -1,33 +1,13 @@
 class Solution {
-    Boolean istriangle(int a,int b,int c){
-        if((a+b)>c){
-            if((a+c)>b){
-                if((b+c)>a){
-                    return true;
-                }
-            }
-
-        }
-        return false;
-    }
     public int largestPerimeter(int[] nums) {
-        if(nums.length<3) return 0;
-        int Max=0,perimeter;
         Arrays.sort(nums);
-        for(int i=nums.length-1;i>1;i--){
-           int lp=i-1,rp=lp-1;
-            while(rp>=0){
-                if(istriangle(nums[i],nums[lp],nums[rp])){
-                    perimeter=nums[i]+nums[lp]+nums[rp];
-                    Max=Math.max(Max,perimeter);
-    
-                }
-                lp--;
-                rp--;
+        for(int i=nums.length-1;i>=2;i--){
+            if(nums[i-1]+nums[i-2]>nums[i]){
+                return nums[i]+nums[i-1]+nums[i-2];
             }
         }
-        if(Max==0) return 0;
-        return Max;
+        return 0;
+        
 
         
     }

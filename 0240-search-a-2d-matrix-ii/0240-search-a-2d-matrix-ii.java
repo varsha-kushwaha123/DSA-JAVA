5e9788153648f5852ac9e030d910fa0847bf1b86
varsha-kushwaha123 <aec.cse.2024varshakushwaha@gmail.com>
2026-05-25 10:11:02 +0530

    Time: 3 ms (88.29%), Space: 48 MB (81.37%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row=matrix.length-1;
+        int col=0;
+        while(row>=0 && col<matrix[0].length){
+            if(target==matrix[row][col]){
+                return true;
+            }
+            else if(target<matrix[row][col]){
+                row--;
+            }
+            else{
+                col++;
+            }
+        }
+        return false;
+
+        
+    }
+}

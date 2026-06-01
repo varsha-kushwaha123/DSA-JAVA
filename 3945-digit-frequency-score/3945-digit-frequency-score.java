e3259d6620ea8815a2ae606c8f09234582fb0213
varsha-kushwaha123 <aec.cse.2024varshakushwaha@gmail.com>
2026-06-01 11:33:54 +0530

    Time: 1 ms (100%), Space: 42.4 MB (100%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,16 +1,12 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-        HashMap<Character,Integer>map=new HashMap<>();
-        String nums=Integer.toString(n);
-        for(Character num:nums.toCharArray()){
-            map.put(num,map.getOrDefault(num,0)+1);
-
+        int sum=0;
+        while(n>0){
+            int x=n%10;
+            sum+=x;
+            n=n/10;
         }
-        int count=0;
-        for(Character key:map.keySet()){
-            count+=((key-'0')*map.get(key));
-        }
-           return count;
+        return sum;
     }
  
 }

a1fc0a916a9460fbeda001c89a0f248509dca4e8
varsha-kushwaha123 <aec.cse.2024varshakushwaha@gmail.com>
2026-06-20 19:16:41 +0530

    Time: 0 ms (100%), Space: 43.6 MB (23.7%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,37 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int count0=0;
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                count0++;
+            }
+            else if(nums[i]==1){
+                count1++;
+            }
+            else{
+                count2++;
+            }
+            
+        }
+        int j=0;
+        while(count0>0){
+            nums[j]=0;
+            count0--;
+            j++;
+        }
+        while(count1>0){
+            nums[j]=1;
+            count1--;
+            j++;
+        }
+        while(count2>0 && j<=nums.length){
+            nums[j]=2;
+            count2--;
+            j++;
+        }
+        
         
     }
 }

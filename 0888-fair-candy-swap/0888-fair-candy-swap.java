6db9a5faacf2cd6a462d0640046c0f90f3073329
varsha-kushwaha123 <aec.cse.2024varshakushwaha@gmail.com>
2026-07-15 20:37:44 +0530

    Time: 8 ms (97.84%), Space: 48.7 MB (22.53%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.java
+++ 0888-fair-candy-swap/0888-fair-candy-swap.java
@@ -2,6 +2,7 @@ class Solution {
     public int[] fairCandySwap(int[] a, int[] b) {
         
         int[]answer=new int[2];
+        Set<Integer>set=new HashSet();
         int sumA=0;
         int sumB=0;
         for(int i=0;i<a.length;i++){
@@ -10,14 +11,15 @@ class Solution {
         }
         for(int i=0;i<b.length;i++){
             sumB+=b[i];
+            set.add(b[i]);
         }
+        int finalsum=(sumA +sumB)/2;
         for(int i=0;i<a.length;i++){
-            for(int j=0;j<b.length;j++){
-                if(sumA-a[i]+b[j]==sumB-b[j]+a[i]){
-                    answer[0]=a[i];
-                    answer[1]=b[j];
-                    return answer;
-                }
+            int value=finalsum-sumA + a[i];
+            if(set.contains(value)){
+                answer[0]=a[i];
+                answer[1]=value;
+                return answer;
             }
         }
         return answer;

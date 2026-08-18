541f61e9379ffee79dda41bf3a0a60263113dae6
varsha-kushwaha123 <aec.cse.2024varshakushwaha@gmail.com>
2026-08-18 18:35:33 +0530

    Time: 0 ms (100%), Space: 44 MB (86.06%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        List<String>list=new ArrayList<>();
+     List<String>list=new ArrayList<>();
         int j=0;
         for(int i=1;i<=n && j<target.length;i++){
             list.add("Push");
